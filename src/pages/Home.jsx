94727eb8b210f0ef7c9a9085e8fdb690ad3c5295
danielkuzminski <dankuzmina@gmail.com>
2023-10-24 11:44:43 +0200

    building home page, in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,32 @@
 //styles
-import './Home.css'
+import "./Home.css"
 
-//assets
-import Ja from '../assets/ja.jpg'
+//router
+import { Link } from "react-router-dom"
+
+
 
 export default function Home() {
-  return (
-    <div>
-      <img className='my-picture' src={Ja} alt="" />
-    </div>
-  )
+	return (
+		<div className='home-container'>
+			<div className='home-container-box box-1'>
+				<Link className="box-title" to='/gallery'>
+					<h2>galeria</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, ex?</p>
+				</Link>
+			</div>
+			<div className='home-container-box box-2'>
+				<Link className="box-title">
+					<h2>miejsca</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, ex?</p>
+				</Link>
+			</div>
+			<div className='home-container-box box-3'>
+				<Link className="box-title" to='/about'>
+					<h2>o mnie</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia, ex?</p>
+				</Link>
+			</div>
+		</div>
+	)
 }
